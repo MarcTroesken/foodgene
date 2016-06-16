59cdf074608200af21c07fd209a0c458b931721a
Marc Troesken <m.troesken@mail.de>
2016-06-16 15:09:27 +0200

    ready for testing

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ var Config = {};
 // -------------- Config your stuff here -------------//
 
 // Define here the url to get all of your categories
-Config.categoriesUrl = 'http://localhost:8000/categories';
+Config.categoriesUrl = 'http://testapp.marc-troesken.de/categories';
 
 // Define here the url to add the mix to the cart
-Config.cartUrl = 'http://localhost:8000/add-to-cart';
+Config.cartUrl = 'http://testapp.marc-troesken.de/add-to-cart';
 
 // -------------------- End config --------------------//
 
